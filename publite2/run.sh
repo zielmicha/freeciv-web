7397324a96419e8488065ce5c7b270f284056b79
Michał Zieliński <michal@zielinscy.org.pl>
2013-10-06 19:38:42 +0200

    remove publite2

--- publite2/run.sh
+++
@@ -1,7 +0,0 @@
-
-export LANG=C
-FREECIV_SAVE_PATH="~/freeciv-build/resin/webapps/ROOT/savegames/"
-export FREECIV_SAVE_PATH
-
-nohup python3.3 publite2_single.py  &
-nohup python3.3 publite2_multi.py  &
